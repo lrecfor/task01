41ee9ff913d41d8fa8afd7fb49bbc33058c88534
lrecfor <lalarecfor@gmail.com>
2023-09-28 15:12:19 +0500

    replaced the selenium with the arsenic, updated class driver

--- bot.py
+++ bot.py
@@ -53,37 +53,24 @@ async def authorisation(message: types.Message):
 async def sign_in(message: types.Message, state: FSMContext):
     await state.update_data(username=message.text)
     data = await state.get_data()
+    username = None
+    password = None
     try:
         user_data = db.get(data["username"])
         username = user_data.username
         password = fernet.decrypt(user_data.password).decode('utf-8')
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
-        await message.answer("Error.")
+    except Exception:
+        await message.answer("Account was not found. Check your login and try again or create an account.")
         await state.finish()
-    driver = Driver()
-
-    # field filling
-    # driver.find_element("xpath", '//*[@id="id_login"]').send_keys(username)
-    # driver.find_element("xpath", '//*[@id="id_password"]').send_keys(password)
-    # # submit button clicking
-    # driver.find_element("xpath", '/html/body/div[1]/main/div/div[2]/div/form/input[5]').click()
+        return
     try:
-        await driver.open_browser_for_sign_in()
-        await driver.fill_element('#id_login', username)
-        await driver.fill_elements('#id_password', password)
-        await driver.click_element('.btn')
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
-
-    # error checking
-    try:
-        if (await driver.find_element('.msg') ==
-                "Проверьте правильность написания логина и пароля"):
-            await message.answer("Account was not found. Check your login and try again or create an account.")
-    except NoSuchElementException:  # no errors
+        driver = Driver()
+        await driver.open_browser_for_sign_in(username, password)
         db.add(Auth(user_id=user_data.id, authorization_time=datetime.now()))
         await message.answer("You signed in successfully.")
+    except Exception:
+        await message.answer("Something was wrong. Please try again later.")
+
     await state.finish()
 
 
@@ -136,31 +123,11 @@ async def create_acc(message: types.Message, state: FSMContext):
     driver = Driver()
     try:
         await driver.open_browser_for_cr(data)
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
-
-    # error checking
-    try:
-        error_text = ""
-        if (await driver.find_element('.msg') ==
-                "Пользователь с таким логином уже существует"):
-            error_text = f"Account {data['username']} already exists. Please, use another login or sign in."
-        elif (await driver.find_element('.msg') ==
-              "Пользователь с такой почтой уже существует"):
-            error_text = f"Account with e-mail {data['email']} already exists. Please, use another e-mail or sign in."
-        elif (await driver.find_element('.msg') ==
-              "Введите правильный адрес электронной почты"):
-            error_text = f"Incorrect e-mail {data['email']}"
-        elif (await driver.find_element('.msg') ==
-              "Пароль должен быть больше 5 символов"):
-            error_text = f"Password should be more than 5 characters"
-
-        await message.answer(error_text)
-    except NoSuchElementException:  # account was created without errors
         password = fernet.encrypt(data["password1"].encode('utf-8')).decode('utf-8')
         db.add(Reg(username=data['username'], password=password, registration_time=datetime.now()))
         await message.answer(f"Account {data['username']} was created successfully")
-
+    except Exception as e:
+        await message.answer("Something was wrong. Please try again.")
     await state.finish()
 
 
--- driver.py
+++ driver.py
@@ -19,19 +19,6 @@ class InputError(Exception):
     pass
 
 
-# async def ret_error(error_text):
-#     if error_text == "Пользователь с таким логином уже существует":
-#         raise InputError(f"Driver.open_browser_for_cr: Error: Account already exists. Please, use another login or "
-#                          f"sign in.")
-#     elif error_text == "Пользователь с такой почтой уже существует":
-#         raise InputError(f"Driver.open_browser_for_cr: Error: Account with e-mail already exists. Please, use another "
-#                          f"e-mail or sign in.")
-#     elif error_text == "Введите правильный адрес электронной почты":
-#         raise InputError(f"Driver.open_browser_for_cr: Error: Incorrect e-mail")
-#     elif error_text == "Пароль должен быть больше 5 символов":
-#         raise InputError(f"Driver.open_browser_for_cr: Error: Password should be more than 5 characters")
-
-
 class Driver:
     def __init__(self):
         if sys.platform.startswith('win'):
@@ -50,13 +37,16 @@ class Driver:
 
     async def find_element(self, selector):
         try:
-            await self.session.get_element(selector)
+            element = await self.session.get_element(selector)
         except Exception:
-            raise NoSuchElementException(f"Driver.fill_element: Error: Unable to locate element '{selector}'")
+            raise NoSuchElementException(f"Driver.find_element: Error: Unable to locate element '{selector}'")
 
     async def fill_element(self, selector, data_):
-        field = await self.session.get_element(selector)
-        await field.send_keys(data_)
+        try:
+            field = await self.session.get_element(selector)
+            await field.send_keys(data_)
+        except Exception:
+            raise NoSuchElementException(f"Driver.fill_element: Error: Unable to locate element '{selector}'")
 
     async def fill_elements(self, class_name, data_):
         input_fields = await self.session.get_elements(class_name)
@@ -66,56 +56,30 @@ class Driver:
             field_value = data_.get(await input_field.get_attribute('name'))
             await input_field.send_keys(field_value)
 
-    async def open_browser_for_sign_in(self):
-        async with (get_session(services.Geckodriver(binary=self.geckodriver), browsers.Firefox()) as self.session):
-            await self.session.get('http://cappa.csu.ru/')
-            await self.click_element('.profile__bar-login')
+    async def open_browser_for_sign_in(self, username, password):
+        try:
+            async with (get_session(services.Geckodriver(binary=self.geckodriver), browsers.Firefox()) as self.session):
+                await self.session.get('http://cappa.csu.ru/')
+                await self.click_element('.profile__bar-login')
+                await self.fill_element('#id_login', username)
+                await self.fill_element('#id_password', password)
+                await self.click_element('.btn')
+
+        except Exception as e_:
+            raise SigninError(e_)
 
     async def open_browser_for_cr(self, data_):
-        async with (get_session(services.Geckodriver(binary=self.geckodriver), browsers.Firefox()) as self.session):
-            await self.session.get('http://cappa.csu.ru/')
-            await self.click_element('.profile__bar-login')
-            await self.click_element('.profile__bar-login')
-            await self.click_element('.signup__link')
-
-            try:
+        try:
+            async with (get_session(services.Geckodriver(binary=self.geckodriver), browsers.Firefox()) as self.session):
+                await self.session.get('http://cappa.csu.ru/')
+                await self.click_element('.profile__bar-login')
+                await self.click_element('.signup__link')
                 await self.fill_elements('.form-control', data_)
                 await self.click_element('.btn')
-            except NoSuchElementException as e:
-                raise RegistrationError(e)
-
-            try:
-                await self.find_element('.btn')
-            except NoSuchElementException as e:
-                print(e)
-                raise RegistrationError(e)
-        print("Done")
-
-# async def open_webpage():
-#     if sys.platform.startswith('win'):
-#         GECKODRIVER = './geckodriver.exe'
-#     else:
-#         GECKODRIVER = './geckodriver'
-#
-#     service = services.Geckodriver(binary=GECKODRIVER)
-#     browser = browsers.Firefox()
-#
-#     async with (get_session(service, browser) as session):
-#         await session.get('http://cappa.csu.ru/')
-#
-#         sign_in_button = await session.get_element('.profile__bar-login')
-#         await sign_in_button.click()
-
-data = {
-    'username': 'test_user5',
-    'email': 'XXXXXXXX5@gmail.com',
-    'password1': 'test_password',
-    'password2': 'test_password',
-    'first_name': 'test_first_name',
-    'last_name': 'test_last_name',
-}
-driver = Driver()
-try:
-    asyncio.run(driver.open_browser_for_cr(data))
-except RegistrationError as e:
-    print(e)
+                try:
+                    await self.find_element('.btn')
+                    raise InputError(f"Driver.open_browser_for_cr: Error: input data is wrong")
+                except NoSuchElementException:
+                    pass
+        except Exception as e_:
+            raise RegistrationError(e_)
