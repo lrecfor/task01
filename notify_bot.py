5420d0597ab62dc19af6c340f21a7719c6e0c957
lrecfor <lalarecfor@gmail.com>
2023-09-29 14:26:31 +0500

    add support for asynс access to db

--- notify_bot.py
+++ notify_bot.py
@@ -2,12 +2,12 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import config
-from database import Database, Reg, Auth
-from sqlalchemy import create_engine
-from sqlalchemy import Integer, cast
-from sqlalchemy.orm import sessionmaker
+from database import SessionManager
+from models import Reg, Auth
+from sqlalchemy import Integer, cast, select
 import logging
 import os
+import models
 
 storage = MemoryStorage()
 bot = Bot(token=config.N_TOKEN)
@@ -20,36 +20,41 @@ logging.basicConfig(filename=config.LOGPATH + config.N_LOGNAME,
                     level=logging.ERROR)
 dp.middleware.setup(LoggingMiddleware())
 
-db = Database()
-
 
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     # function for start command handling
     # get data from database in loop and send message to user when new user registered or authenticated
     await message.answer("Start working")
-    url = config.DATABASE_URL
-    engine = create_engine(url, echo=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    reg_count = session.query(Reg).count()
-    auth_count = session.query(Auth).count()
 
-    while True:
-        if session.query(Reg).count() != reg_count:
-            reg_count = session.query(Reg).count()
-            data = session.query(Reg).order_by(Reg.id.desc()).first()
-            await message.answer('New user registered: \n' + 'id: ' + str(data.id) + '\nusername: ' + data.username
-                                 + '\nregistration_time: ' + data.registration_time)
+    try:
+        async with SessionManager() as session:
+            reg_count = len((await session.execute(select(models.Reg))).all())
+            auth_count = len((await session.execute(select(models.Auth))).all())
 
-        if session.query(Auth).count() != auth_count:
-            auth_count = session.query(Auth).count()
-            data = session.query(Auth).order_by(Auth.id.desc()).first()
-            username = session.query(Reg).join(Auth, cast(Auth.user_id, Integer) == Reg.id).filter(
-                data.user_id == cast(Reg.id, Integer)).first().username
-            await message.answer('New user authenticated: \n' + 'user_id: ' + data.user_id + '\nusername: ' + username
-                                 + '\nauthorization_time: ' + data.authorization_time)
+            while True:
+                if len((await session.execute(select(models.Reg))).all()) != reg_count:
+                    reg_count = len((await session.execute(select(models.Reg))).all())
+                    stmt = select(models.Reg).order_by(Reg.id.desc())
+                    data = (await session.execute(stmt)).scalars().first()
+                    await message.answer('New user registered: \n' + 'id: ' + str(data.id) + '\nusername: ' + data.username
+                                         + '\nregistration_time: ' + data.registration_time)
+
+                if len((await session.execute(select(models.Auth))).all()) != auth_count:
+                    auth_count = len((await session.execute(select(models.Auth))).all())
+                    stmt = select(models.Auth).order_by(Auth.id.desc())
+                    data = (await session.execute(stmt)).scalars().first()
+                    stmt = select(models.Reg).join(Auth, cast(Auth.user_id, Integer) == Reg.id).filter(
+                        int(data.user_id) == cast(Reg.id, Integer))
+                    username = (await session.execute(stmt)).scalars().first().username
+                    await message.answer('New user authenticated: \n' + 'user_id: ' + data.user_id + '\nusername: ' + username
+                                         + '\nauthorization_time: ' + data.authorization_time)
+    except Exception as e:
+        await message.answer("Something was wrong. Bot stopped working.")
 
 
 def execute():
     executor.start_polling(dp, skip_updates=True)
+
+
+execute()
